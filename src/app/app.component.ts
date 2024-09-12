2ef5d2f43f2826ac291dce5946b0da7513fefb91
parekh <w0846147@myscc.ca>
2024-09-11 23:20:59 -0700

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Rushabh-Parekh-Learning-Angular';
+  name:string = 'Rushabh';
+  studentNo:string = 'woi';
 }
