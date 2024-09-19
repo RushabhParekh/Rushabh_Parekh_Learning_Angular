e022d201697ef4d257e7eb327106b35d8ae91829
parekh <w0846147@myscc.ca>
2024-09-18 20:51:38 -0700

    Created starter Angular file complited the values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,15 @@ import {NgForOf} from "@angular/common";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  user1 :
+  user1 : User = {id: 1, firstName: "Rushabh", lastName: "Parekh",course: "Computer Programming", studentNo: 14567891,isAdmin: false};
+  user2 : User = {id: 2, firstName: "Megh", lastName: "Patel",course: "Computer System Technician", studentNo: 62365,isAdmin: false};
+  user3 : User = {id: 3, firstName: "Meetraj", lastName: "Cavda",course: "Computer Programming", studentNo: 2961465,isAdmin: false};
+  user4 : User = {id: 4, firstName: "Shami", lastName: "Rathhod",course: "Computer Programming", studentNo: 2269465,isAdmin: false};
+  user5 : User = {id: 5, firstName: "Virat", lastName: "Patel",course: "Computer Programming", studentNo: 23696465,isAdmin: false};
+  user6 : User = {id: 6, firstName: "Mahendra", lastName: "Patel",course: "Civil Enginnering", studentNo: 256465,isAdmin: false};
+
+//Putting the values in the user list
+  userList: User[]=[this.user1,this.user2,this.user3,this.user4,this.user5,this.user6]
+
 }
+
