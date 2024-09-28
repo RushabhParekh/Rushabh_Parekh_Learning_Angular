37858794abae880cfc54133e1488082994fde8aa
parekh <w0846147@myscc.ca>
2024-09-27 20:09:03 -0700

    used one binding and 4 item list stored in array and 4 content list item tags in the content list html.

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {User} from "../../models/User";
-import {NgForOf, NgIf} from "@angular/common";
+import {NgClass, NgForOf} from "@angular/common";
+import {UserListItemComponent} from "../user-list-item/user-list-item.component";
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    UserListItemComponent,
+    NgClass
   ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
@@ -17,10 +19,8 @@ export class UserListComponent {
   user2 : User = {id: 2, firstName: "Megh", lastName: "Patel",course: "Computer System Technician", studentNo: 62365,isAdmin: false};
   user3 : User = {id: 3, firstName: "Meetraj", lastName: "Cavda",course: "Computer Programming", studentNo: 2961465,isAdmin: false};
   user4 : User = {id: 4, firstName: "Shami", lastName: "Rathhod",course: "Computer Programming", studentNo: 2269465,isAdmin: false};
-  user5 : User = {id: 5, firstName: "Virat", lastName: "Patel",course: "Computer Programming", studentNo: 23696465,isAdmin: false};
-  user6 : User = {id: 6, firstName: "Mahendra", lastName: "Patel",course: "Civil Enginnering", studentNo: 256465,isAdmin: false};
 
 //keeping the valuse in User list
-  userList: User[]=[this.user1,this.user2,this.user3,this.user4,this.user5,this.user6]
+  userList: User[]=[this.user1,this.user2,this.user3,this.user4]
 
 }
