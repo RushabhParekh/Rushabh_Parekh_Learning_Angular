34debe4094afc306b96f6af13c81a560dffafdae
parekh <w0846147@myscc.ca>
2024-11-08 15:33:33 -0800

    step4 and step5 and step6 completed of assignment7

---
+++ src/app/Services/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import {User} from "../../shared/models/User";
+
+
+export class InMemoryDataService{
+  createDb(): { user3: User[] } {
+
+    const user3: User[]= [
+      { id: 1, firstName: "Rushabh", lastName: "Parekh", course: "Computer Programming", studentNo: 14567891, isAdmin: false, images: "/assests/javascript1.jpg"},
+      {  id: 2, firstName: "Megh", lastName: "Patel", course: "Computer System Technician", studentNo: 62365, isAdmin: false, images: "/assests/javascript2.webp"},
+      {  id: 3, firstName: "Meetraj", lastName: "Cavda",course: "Computer Programming", studentNo: 2961465, isAdmin: false, images: "/assests/javascript3.jpg"},
+      {  id: 4, firstName: "Shami", lastName: "Rathhod", course: "Computer Programming", studentNo: 2269465, isAdmin: false, images: "/assests/javascript4.jpg"},
+      { id: 5, firstName: "Virat", lastName: "Patel", course: "Computer Programming", studentNo: 23696465, isAdmin: false, images: "/assests/javascript5.jpg"},
+      {  id: 6, firstName: "Mahendra", lastName: "Patel", course: "Civil Enginnering", studentNo: 256465, isAdmin: false, images:"/assests/javascript6.jpg"},
+    ];
+    return {user3};
+
+  }
+}
