becd2f965b9d997fffc2465ee5510bb3013cd8a1
parekh <w0846147@myscc.ca>
2024-11-29 18:18:30 -0800

    completed step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import {User} from "../shared/models/User";
 import {NgForOf, NgIf} from "@angular/common";
 import {UserListComponent} from "./user-list/user-list.component";
 import {UserListItemComponent} from "./user-list-item/user-list-item.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgForOf, UserListComponent,UserListItemComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, NgIf, NgForOf, UserListComponent, UserListItemComponent, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
